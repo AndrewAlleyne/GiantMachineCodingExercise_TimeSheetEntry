e9f8cfa244b7a6ab51254a16b22de790b7204c43
Andrew Alleyne <andrew.alleyne.jr@gmail.com>
2023-01-30 13:47:42 -0500

    Fixed ResponseEntity, Testing coverage

--- src/main/java/com/example/giantmachinecodingexercise_timesheetentry/GiantMachineCodingExerciseTimeSheetEntryApplication.java
+++ src/main/java/com/example/giantmachinecodingexercise_timesheetentry/GiantMachineCodingExerciseTimeSheetEntryApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GiantMachineCodingExerciseTimeSheetEntryApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(GiantMachineCodingExerciseTimeSheetEntryApplication.class, args);
     }
